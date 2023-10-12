35e3124151a112ac996ed9d7385dcd80d5fb920d
tompa <tjp225@lehigh.edu>
2023-10-12 12:50:11 -0400

    Treat backend as root of backend-dokku

---
+++ src/main/java/edu/lehigh/cse216/knights/backend/Idea.java
@@ -0,0 +1,67 @@
+package edu.lehigh.cse216.knights.backend;
+
+// import java.util.Date;
+
+/**
+ * Idea holds a row of information.  A row of information consists of
+ * an identifier, a string for "content", and an integer for likeCount.
+ * 
+ * Because we will ultimately be converting instances of this object into JSON
+ * directly, we need to make the fields public.  That being the case, we will
+ * not bother with having getters and setters... instead, we will allow code to
+ * interact with the fields directly.
+ */
+public class Idea {
+    /**
+     * The unique identifier associated with this element.  It's final, because
+     * we never want to change it.
+     */
+    public final int mId;
+
+    /**
+     * The content for this idea entry
+     */
+    public String mContent;
+
+    /**
+     * The like count for this idea entry
+     */
+    public int mLikeCount;
+
+    /**
+     * The creation date for this idea entry.  Once it is set, it cannot be 
+     * changed. Not yet implemented in phase 1
+     */
+    // public final Date mCreated;
+
+    /**
+     * Create a new Idea with the provided id and title/content, and a 
+     * creation date based on the system clock at the time the constructor was
+     * called. Like count always starts at 0.
+     * 
+     * @param id The id to associate with this row.  Assumed to be unique 
+     *           throughout the whole program.
+     * 
+     * @param title The title string for this row of idea
+     * 
+     * @param content The content string for this row of idea
+     */
+    public Idea(int id, String content) {
+        mId = id;
+        mContent = content;
+        mLikeCount = 0;
+        // mCreated = new Date();
+    }
+
+    /**
+     * Copy constructor to create one Idea from another.
+     * Currently not used in production code, just in the MockDatabase.
+     */
+    Idea(Idea idea) {
+        mId = idea.mId;
+        // NB: Strings and Dates are immutable, so copy-by-reference is safe
+        mContent = idea.mContent;
+        mLikeCount = idea.mLikeCount;
+        // mCreated = idea.mCreated;
+    }
+}
