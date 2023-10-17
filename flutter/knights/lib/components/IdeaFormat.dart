fa1a123af9c6e82c6dc4b2de9efa1cddc852865c
Joeyme5225 <93407834+Joeyme5225@users.noreply.github.com>
2023-10-17 17:55:17 -0400

    refactored code. added pages folder to hold widgets that are full pages

--- flutter/knights/lib/components/IdeaFormat.dart
+++ flutter/knights/lib/components/IdeaFormat.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:like_button/like_button.dart';
 import 'package:knights/net/webRequests.dart';
 
--- flutter/knights/lib/net/webRequests.dart
+++ flutter/knights/lib/net/webRequests.dart
@@ -1,7 +1,7 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:developer' as developer;
-import 'package:knights/components/IdeaFormat.dart';
+
 import 'package:knights/models/Idea.dart';
 
 Future<bool> onDislikeButtonTapped(int id) async{
