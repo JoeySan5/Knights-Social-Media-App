399c9525bb8b5f8434805b15432bc64b0a70b667
tompa <tjp225@lehigh.edu>
2023-10-17 12:26:16 -0400

    Add CORS enabling

--- backend/src/main/java/edu/lehigh/cse216/knights/backend/App.java
+++ backend/src/main/java/edu/lehigh/cse216/knights/backend/App.java
@@ -9,6 +9,9 @@ import java.util.Map;
 // Import Google's JSON library
 import com.google.gson.*;
 
+import edu.lehigh.cse216.knights.backend.Idea;
+import edu.lehigh.cse216.knights.backend.IdeaRequest;
+
 /**
  * For now, our app creates an HTTP server that can only get and add data.
  */
@@ -41,6 +44,14 @@ public class App
             Spark.staticFileLocation("/web");
         } else {
             Spark.staticFiles.externalLocation(static_location_override);
+        }
+
+        // enable CORS if the environment specifies to do so
+        if ("True".equalsIgnoreCase(System.getenv("CORS_ENABLED"))) {
+            final String acceptCrossOriginRequestsFrom = "*";
+            final String acceptedCrossOriginRoutes = "GET,PUT,POST,DELETE,OPTIONS";
+            final String supportedRequestHeaders = "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin";
+            enableCORS(acceptCrossOriginRequestsFrom, acceptedCrossOriginRoutes, supportedRequestHeaders);
         }
 
         // Set up a route for serving the main page
@@ -188,5 +199,38 @@ public class App
             return Integer.parseInt(processBuilder.environment().get(envar));
         }
         return defaultVal;
+    }
+
+    /**
+     * Set up CORS headers for the OPTIONS verb, and for every response that the
+     * server sends.  This only needs to be called once.
+     * 
+     * @param origin The server that is allowed to send requests to this server
+     * @param methods The allowed HTTP verbs from the above origin
+     * @param headers The headers that can be sent with a request from the above
+     *                origin
+     */
+    private static void enableCORS(String origin, String methods, String headers) {
+        // Create an OPTIONS route that reports the allowed CORS headers and methods
+        Spark.options("/*", (request, response) -> {
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+            return "OK";
+        });
+
+        // 'before' is a decorator, which will run before any 
+        // get/post/put/delete.  In our case, it will put three extra CORS
+        // headers into the response
+        Spark.before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", origin);
+            response.header("Access-Control-Request-Method", methods);
+            response.header("Access-Control-Allow-Headers", headers);
+        });
     }
 }
