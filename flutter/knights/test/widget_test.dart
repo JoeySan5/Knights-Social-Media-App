0d5c241d08b907b7b5d8ef747d41c8debc81fc55
Joeyme5225 <93407834+Joeyme5225@users.noreply.github.com>
2023-10-17 20:45:04 -0400

    cleaned test

--- flutter/knights/test/widget_test.dart
+++ flutter/knights/test/widget_test.dart
@@ -44,45 +44,5 @@ void main() {
  
 });
 
-  // Define a test. The TestWidgets function also provides a WidgetTester
-  // to work with. The WidgetTester allows you to build and interact
-  // with widgets in the test environment.
-  // testWidgets('Say your piece button is present and triggers navigation after tapped', (WidgetTester tester) async{
-  //   final mockObserver = MockNavigatorObserver();
-  //   //builds widget
-  //   await tester.pumpWidget(
-  //     MaterialApp(
-  //       home: MyHomePage(),
-  //       navigatorObservers: [mockObserver],
-  //     ),
-  //   );
-  //   expect(find.byType(ElevatedButton), findsOneWidget);
-  //   await tester.tap(find.byType(ElevatedButton));
-  //   await tester.pumpAndSettle();
 
-  //   /// Verify that a push event happened
-  //   verify(mockObserver.didPush(any, any));
-
-  //   /// You'd also want to be sure that your page is now
-  //   /// present in the screen.
-  //   expect(find.byType(MessagePage), findsOneWidget);
-  // });
-
-
-  // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-  //   // Build our app and trigger a frame.
-  //   await tester.pumpWidget(const MyApp());
-
-  //   // Verify that our counter starts at 0.
-  //   expect(find.text('0'), findsOneWidget);
-  //   expect(find.text('1'), findsNothing);
-
-  //   // Tap the '+' icon and trigger a frame.
-  //   await tester.tap(find.byIcon(Icons.add));
-  //   await tester.pump();
-
-  //   // Verify that our counter has incremented.
-  //   expect(find.text('0'), findsNothing);
-  //   expect(find.text('1'), findsOneWidget);
-  // });
 }
