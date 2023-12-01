4b17611cbefab91f85d2f8850cd6fa60def33ee3
Adivi Karawat <adk225@lehigh.edu>
2023-12-01 16:50:25 -0500

    added links and files to comments

--- flutter/knights/lib/components/detailed_post_format.dart
+++ flutter/knights/lib/components/detailed_post_format.dart
@@ -1,13 +1,17 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:knights/models/DetailedPost.dart';
 import 'package:knights/net/web_requests.dart';
-import 'package:url_launcher/url_launcher.dart';
+// import 'package:url_launcher/url_launcher.dart';
 
-
-import 'package:knights/models/User.dart';
-import 'package:knights/net/web_requests.dart';
-import 'package:knights/pages/home_page.dart';
-import 'package:knights/components/idea_format.dart';
+// import 'package:knights/models/User.dart';
+// import 'package:knights/net/web_requests.dart';
+// import 'package:knights/pages/home_page.dart';
+// import 'package:knights/components/idea_format.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
 /// this class is to format detailed post view
@@ -29,6 +33,13 @@ class _DetailedPostFormat extends State<DetailedPostFormat> {
   late Future<DetailedPost> _futureDetailedPostFormat;
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
+  @override
+  void initState() {
+    super.initState();
+    _futureDetailedPostFormat =
+        fetchDetailedPost(widget.mId, widget.sessionKey);
+  }
+
   Future<void> _launchUrl(String url) async {
     if (await canLaunchUrlString(url)) {
       await launchUrlString(url);
@@ -39,19 +50,32 @@ class _DetailedPostFormat extends State<DetailedPostFormat> {
     }
   }
 
-
-  @override
-  void initState() {
-    super.initState();
-    _futureDetailedPostFormat =
-        fetchDetailedPost(widget.mId, widget.sessionKey);
-  }
-
   @override
   void retry() {
     super.initState();
     _futureDetailedPostFormat =
         fetchDetailedPost(widget.mId, widget.sessionKey);
+  }
+
+  File? _selectedFile;
+  Future<void> pickFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+    if (result != null) {
+      setState(() {
+        _selectedFile = File(result.files.single.path!);
+        String fileName = result.files.first.name;
+        print('selected fileName: $fileName');
+        Uint8List? fileBytes = result.files.first.bytes;
+        String base64 = base64Encode(fileBytes!);
+        print('base64 file bytes: $base64');
+      });
+    } else {
+      // User canceled the picker
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('No file selected')),
+      );
+    }
   }
 
   /// now we want to read in data from dokku using get and parse json into detailed_post objects
@@ -64,6 +88,7 @@ class _DetailedPostFormat extends State<DetailedPostFormat> {
   @override
   Widget build(BuildContext context) {
     String newComment = "";
+    String link = "";
     return Form(
         key: _formKey,
         child: Center(
@@ -92,19 +117,21 @@ class _DetailedPostFormat extends State<DetailedPostFormat> {
                         children: [
                           Padding(
                             padding: const EdgeInsets.all(8.0),
-                            child: snapshot.data!.mLink != null && snapshot.data!.mLink!.isNotEmpty
-                  ? InkWell(
-                      child: Text(
-                        'Open Link: ${snapshot.data!.mLink}',
-                        style: TextStyle(
-                          color: Colors.blue,
-                          decoration: TextDecoration.underline,
-                        ),
-                      ),
-                      onTap: () =>_launchUrl(snapshot.data!.mLink!),
-                      //onTap: () => launchUrlString(snapshot.data!.mLink!),
-                    )
-                  : Container(), // Empty container when there's no link
+                            child: snapshot.data!.mLink != null &&
+                                    snapshot.data!.mLink!.isNotEmpty
+                                ? InkWell(
+                                    child: Text(
+                                      'Open Link: ${snapshot.data!.mLink}',
+                                      style: TextStyle(
+                                        color: Colors.blue,
+                                        decoration: TextDecoration.underline,
+                                      ),
+                                    ),
+                                    onTap: () =>
+                                        _launchUrl(snapshot.data!.mLink!),
+                                    //onTap: () => launchUrlString(snapshot.data!.mLink!),
+                                  )
+                                : Container(), // Empty container when there's no link
                             // Text(
                             //   'Poster Username: ${snapshot.data!.mPosterUsername}',
                             //   style: const TextStyle(
@@ -136,6 +163,7 @@ class _DetailedPostFormat extends State<DetailedPostFormat> {
                           // Create a widget for each comment using ListView.builder
                           Padding(
                             padding: const EdgeInsets.all(8.0),
+
                             ///builds a list dependongon how many comment objects there are
                             child: ListView.builder(
                               shrinkWrap: true,
@@ -155,14 +183,16 @@ class _DetailedPostFormat extends State<DetailedPostFormat> {
                               },
                             ),
                           ),
+
+                          // add a new comment
                           Padding(
                             padding: const EdgeInsets.all(8.0),
+
                             /// make this form editable because we can add comments
                             child: TextFormField(
-                              
+                              maxLines: 3,
                               style: const TextStyle(
                                   fontSize: 20,
-                              
                                   color: Colors.white,
                                   fontFamily: 'roboto'),
                               decoration: const InputDecoration(
@@ -190,6 +220,49 @@ class _DetailedPostFormat extends State<DetailedPostFormat> {
                               },
                             ),
                           ),
+
+                          // links in comments
+                          Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextFormField(
+                                maxLines: 2, // reduced max lines
+                                decoration: const InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: 'Please write your link here...',
+                                    labelText: 'Please write your link here...',
+                                    labelStyle: TextStyle(
+                                        color: Colors.white,
+                                        fontFamily: 'roboto',
+                                        fontSize: 20),
+                                    hintStyle: TextStyle(color: Colors.white),
+                                    contentPadding: EdgeInsets.all(10)),
+                                onSaved: (String? value) {
+                                  // Save the data when the form is saved
+                                  if (value != null) {
+                                    link = value;
+                                  } else {
+                                    link = "";
+                                  }
+                                  // The key parameter should be unique for each TextFormField
+                                  print('content: $value');
+                                },
+                                style: const TextStyle(color: Colors.white),
+                              )),
+
+                          // Attach file button
+                          ElevatedButton(
+                            onPressed: pickFile,
+                            child: const Text('Attach File'),
+                          ),
+
+                          // Optional: Display the selected file name
+                          if (_selectedFile != null)
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                  "Selected File: ${_selectedFile!.path.split('/').last}"),
+                            ),
+
                           ElevatedButton(
                             style: const ButtonStyle(
                               backgroundColor:
@@ -199,17 +272,18 @@ class _DetailedPostFormat extends State<DetailedPostFormat> {
                               // Validate and save the form
                               if (_formKey.currentState!.validate()) {
                                 _formKey.currentState!.save();
-                                
+
                                 print('Form saved!');
+
                                 /// posts comment using sessionkeyand the text entered and ideaId
                                 postComment(newComment, widget.sessionKey,
                                     snapshot.data!.mId);
                                 print('updated user profile!');
                               }
                             },
-                            child: const Text('Post Comment'),
+                            child: const Text('Add Comment'),
                           ),
-                          
+
                           ElevatedButton(
                             style: const ButtonStyle(
                               backgroundColor:
